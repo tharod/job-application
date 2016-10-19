5d00c415eac61fb634fb8f8c34d574de5cc9969c
Rajesh Harod <tharod.rails@gmail.com>
2016-10-19 13:49:40 +0530

    hamburger navigation

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-// import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import Immutable from 'immutable';
 
 import * as AuthActions from '../actions/auth';
@@ -21,35 +21,41 @@ class Header extends React.Component {
 
   renderLinks() {
     if (this.props.auth.getIn(['signedIn'])) {
-      return [
-        <li className="nav-item" key={1}>
-          <Link className="nav-link" to="#" onClick={(e)=> this.handleClick(e)}>Logout</Link>
-        </li>
-      ]
+      return (
+        <Nav>
+          <li key={1}>
+            <Link className="nav-link" to="#" onClick={(e)=> this.handleClick(e)}>Logout</Link>
+          </li>
+        </Nav>
+      )
     }
     else{
-       return [<li className="nav-item" key={2}>
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>,
-        <li className="nav-item" key={3}>
-          <Link className="nav-link" to="/signup">SignUp</Link>
-        </li>
-        ]
+      return(
+        <Nav>
+          <li key={2}>
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li key={3}>
+            <Link className="nav-link" to="/signup">SignUp</Link>
+          </li>
+        </Nav>
+      )
     }
   }
 
   render() {
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
             <Link className="navbar-brand" to="/">Job Application</Link>
-          </div>
-           <ul className="nav navbar-nav navbar-right">
-             { this.renderLinks() }
-           </ul>
-        </div>
-      </nav>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          { this.renderLinks() }
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
